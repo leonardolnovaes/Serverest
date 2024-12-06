0216871fdda2050587262988efe92b1c45144f58
Leonardo Novaes <leonardoldenovaes@gmail.com>
2024-12-06 11:07:07 -0300

    correcting allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      require("cypress-allure-plugin")(on, config); // Carrega o plugin Allure
+      allureWriter(on, config);
 
       return config;
     },
