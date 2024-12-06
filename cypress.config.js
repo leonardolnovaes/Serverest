7ba33d0f1323be46a6e07a9574ae4982f5170653
Leonardo Novaes <leonardoldenovaes@gmail.com>
2024-12-06 10:53:53 -0300

    installing allure and create reusable workflows

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}', // Caminho para os testes
     setupNodeEvents(on, config) {
-      // Configurações de eventos aqui, se necessário
+      require("cypress-allure-plugin")(on, config); // Carrega o plugin Allure
+
+      return config;
+    },
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}", // Caminho para os testes
+    reporter: "cypress-allure-plugin", // Define o Allure como reporter
+    reporterOptions: {
+      outputDir: "allure-results", // Diretório onde os resultados dos testes serão salvos
     },
   },
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,6 +24,7 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 import 'cypress-file-upload';
+import 'cypress-allure-plugin';
 
 // cypress/support/commands.js
 
